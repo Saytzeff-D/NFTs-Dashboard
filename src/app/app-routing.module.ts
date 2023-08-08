7ff5dea2eb591522fc76f99e67df7070734f6574
David Atanda <ololadedavid15@gmail.com>
2023-08-08 16:33:54 +0100

    nfts route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { NftsComponent } from './nfts/nfts.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent
+      },
+      {
+        path: 'nfts',
+        component: NftsComponent
       }
     ]
   }
