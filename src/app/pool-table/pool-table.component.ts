45f4a4bcb52077a11b04a7bd1a7df1a2552219ad
David Atanda <ololadedavid15@gmail.com>
2023-08-11 14:45:25 +0100

    pool table

--- src/app/pool-table/pool-table.component.ts
+++ src/app/pool-table/pool-table.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pool-table',
   templateUrl: './pool-table.component.html',
   styleUrls: ['./pool-table.component.css']
 })
-export class PoolTableComponent {
+export class PoolTableComponent implements OnInit {
+  public arr = [1,2,3,4,5,6]
+
+  ngOnInit(): void {
+      
+  }
 
 }
